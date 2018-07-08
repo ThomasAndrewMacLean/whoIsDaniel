05142e0db209cf5a0c6d4ef4235e62bfbc7558ad
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-07-08 23:51:58 +0200

    pics

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { pic: './0.jpg', text: 'Dat ben ikke...' };
+  }
+  switchPic() {
+    let random = ['en ik..', 'en jij???¿?', 'dit ook 🤖', 'dat ben IK', 'uhu...', 'wie ben jij?', 'gijzelf!']
+    let x = Math.floor(Math.random() * 7);
+    
+    let y = Math.floor(Math.random() * 7);
+
+    this.setState({ pic: x + '.jpg', text: random[y] })
+  }
+
+
+
   render() {
     return (
-      <div className="App">
-        <img src="./daniel.jpg" className="daniel" alt="logo" />
+      <div className="App" onClick={() => this.switchPic()} >
+        <img src={this.state.pic} className="daniel" alt="logo" />
         <div className="datbenik">
-          Dat ben ikke...</div>
+          {this.state.text}  </div>
       </div>
     );
   }

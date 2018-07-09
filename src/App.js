cec585b8c42bc469278e8a426f19e7f0a3cce1a2
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-07-09 08:15:48 +0200

    upload foto

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { pic: 0, text: 0, color: 0, left: 40, top: 50 };
+    this.state = { pic: 0, text: 0, color: 0, left: 40, top: 50, camera: 'none' };
   }
   switchPic() {
-
+    //TODO make more dynamic
     let newPic = (1 + this.state.pic + Math.floor(Math.random() * 5)) % 7;
     let newText = (1 + this.state.text + Math.floor(Math.random() * 6)) % 8;
     let newColor = (1 + this.state.color + Math.floor(Math.random() * 3)) % 5;
     let newLeft = (1 + this.state.left + Math.floor(Math.random() * 2)) % 5;
     let newTop = (1 + this.state.top + Math.floor(Math.random() * 2)) % 5;
 
-
-    this.setState({ pic: newPic, text: newText, color: newColor, left: newLeft, top: newTop })
+    this.setState({ pic: newPic, text: newText, color: newColor, left: newLeft, top: newTop, camera: 'flex' })
   }
 
 
@@ -26,15 +25,23 @@ class App extends Component {
     let left = [30, 40, 10, 20, 5];
     let colors = ['deeppink', 'goldenrod', 'yellowgreen', 'black', 'blue'];
     let colorStyle = {
+      position: 'fixed',
       background: colors[this.state.color],
       left: left[this.state.left] + '%',
       top: top[this.state.top] + '%'
     }
+    let cameraStyle = {
+      display: this.state.camera
+    }
+
     return (
-      <div className="App" onClick={() => this.switchPic()} >
-        <img src={`./${this.state.pic}.jpg`} className="daniel" alt="logo" />
+      <div className="App"  >
+        <img src={`./${this.state.pic}.jpg`} className="daniel" alt="logo" onClick={() => this.switchPic()} />
         <div className="datbenik" style={colorStyle}>
           {random[this.state.text]}  </div>
+        <input type="file" id="addPhotoke" />
+        <label style={cameraStyle} className="addPhotoBtn" for="addPhotoke"><span>📷</span>  </label>
+
       </div >
     );
   }
